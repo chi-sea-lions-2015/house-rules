1cbf9107d728fa022ec6ebb2fd556658a88b6774
Cleggameg <Cleggameg@gmail.com>
2015-04-17 17:26:32 -0500

    changed house model

--- app/models/house.rb
+++ app/models/house.rb
@@ -3,7 +3,10 @@ class House < ActiveRecord::Base
   has_one :address, as: :addressable
 
   has_many :housing_assignments
+  has_many :messages, through: :housing_assignments
   has_many :users, through: :housing_assignments
+  has_many :communal_items, through: :housing_assignments
+  has_many :rules, through: :housing_assignments
   has_many :communal_items
   has_many :chores
 
