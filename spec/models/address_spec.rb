a33d542726987a7d047df6940280d9839b31ae02
Cleggameg <Cleggameg@gmail.com>
2015-04-17 16:51:56 -0500

    models partially done, will finish later

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  address = FactoryGirl.build(:address)
+  it { expect(address).to validate_presence_of(:street) }
+  it { expect(address).to validate_presence_of(:city) }
+  it { expect(address).to allow_value('IL').for(:state) }
+  it { expect(address).to validate_length_of(:password)}
+  it { expect(address).to belong_to(:addressable)}
 end
