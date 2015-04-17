cff30de04845357f38b6c575e19ec2ecfa8a2530
Morgan McCrory <morganmccrory@gmail.com>
2015-04-17 10:23:04 -0500

    Clean User model and add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :housing_assignments
   has_many :houses, through: :housing_assignments
-  has_many :property_managers, through: :housing_assignments
+  has_many :messages, through: :housing_assignments
+  has_many :events, through: :housing_assignments
   has_many :issues
   has_many :chore_logs
   has_many :user_promises
-  has_many :rules, through: :housing_assignments
-  has_many :events, through: :housing_assignments
-  has_many :messages, through: :housing_assignments
-  has_many :commununal_items, through: :housing_assignments
-  has_many :chores, through: :chore_logs
-  has_many :addresses, through: :housing_assignments
-  has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, :uniqueness => true, :format => /.+@.+\..+/
+  validates :password_digest, :length => { :minimum => 6 }
 end
