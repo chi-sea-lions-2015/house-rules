2b10b0cec372f49077d38c28450446ccc452a521
z-lake <zachary.austin.harris@gmail.com>
2015-04-17 18:16:10 -0500

    WIP on messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,25 +3,45 @@ class MessagesController < ApplicationController
   def index
     @user = current_user
     @house = House.find_by(id: params[:id])
-    @housing_assignment = HousingAssignment.where()
-    @messages = @house.housing_assignment
+    @housing_assignment = HousingAssignment.where(house_id: @house.id)
+    @messages = @housing_assignment.map do |h_a| 
+      h_a.messages 
+    end
   end
 
   def new
     @user = current_user
+    @house = House.find_by(id: params[:id])
+    @housing_assignment = HousingAssignment.where(house_id: @house.id)
   end
 
   def create
     @user = current_user
-    @housing_assignment = HousingAssignment
-    @message
-  end
+    @house = House.find_by(id: params[:id])
+    @housing_assignment = HousingAssignment.find_by(house_id: @house.id, user_id: @user.id)
+    @message = @user.messages.new(params[:message])
+    if @message.save
+      redirect "index"
+    else
+      flash.now[:error] = "Message did not save"
+      redirect "index"
+     end
+   end
 
   def update
     @user = current_user
+    @house = House.find_by(id: params[:house_id])
+    @housing_assignment = HousingAssignment.find_by(house_id: @house.id, user_id: @user.id)
+    @message = Message.find_by(id: params)
   end
 
   def destroy
   end
+
+private
+
+    def message_params
+      params.require(:message).permit(:content, :picture_url)
+    end
 
 end
