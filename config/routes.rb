1b3d20cce10b729d74581246e04dd28be76abcea
z-lake <zachary.austin.harris@gmail.com>
2015-04-18 21:08:39 -0500

    succesful migration

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   devise_for :user, only: []
 
 
-namespace :v1, defaults: { format: :json } do
+  namespace :v1, defaults: { format: :json } do
     resource :login, only: [:create], controller: :sessions
   end
-end
 
   resources :user_promises
   get    '/'  => 'sessions#new'
