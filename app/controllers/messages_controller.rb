0206a18d045bf51ce14332f44a5dde98630d7342
Morgan McCrory <morganmccrory@gmail.com>
2015-04-19 22:45:23 -0500

    idk

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 # module V1
   class MessagesController < ApplicationController
-    skip_before_action :authenticate_user_from_token!, only: [:index]
+    skip_before_action :authenticate_user_from_token!, only: [:index, :create]
 
     def index
       @house = House.find(params[:house_id])
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-module V1
+# module V1
   class SessionsController < ApplicationController
     skip_before_action :authenticate_user_from_token!
 
@@ -28,4 +28,4 @@ module V1
     end
 
   end
-end
+# end
