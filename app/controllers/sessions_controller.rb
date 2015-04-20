15fc67109eeda64c98fae63c5d1b7829e1deef55
z-lake <zachary.austin.harris@gmail.com>
2015-04-19 22:04:47 -0500

    correct errors with session login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_for_database_authentication(email: params[:email])
     return invalid_login_attempt unless @user
-
+    puts @user
     if @user.valid_password?(params[:password])
       sign_in :user, @user
       render json: @user, serializer: SessionSerializer, root: nil
