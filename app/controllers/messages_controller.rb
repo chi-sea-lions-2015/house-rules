5732700a0014ebb397d4d9e151b4ca6d46b82ebb
cleggameg <morganmccrory@gmail.com>
2015-04-20 19:14:01 -0500

    Working authors

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 # module V1
   class MessagesController < ApplicationController
-    skip_before_action :authenticate_user_from_token!, only: [:index, :create]
+
 
     def index
       @house = House.find(params[:house_id])
@@ -11,10 +11,10 @@
 
   def create
     @house = House.find(params[:house_id])
-    @messages = Message.where(house_id: @house.id)
+    @user = current_user
 
     @message = @house.messages.new(message_params)
-    @message.update_attributes(author: current_user)
+    @message.update_attributes(author: @user.first_name)
 
     if @message.save
       render json: @message, each_serializer: MessagesSerializer
--- app/serializers/messages_serializer.rb
+++ app/serializers/messages_serializer.rb
@@ -1,7 +1,6 @@
 class MessagesSerializer < ActiveModel::Serializer
-  embed :ids, :include => true
+  # embed :ids, :include => true
 
-  attributes :content, :created_at, :id
-  has_one :author, class_name: "User"
-  has_one :house
+  attributes :content, :created_at, :id, :author
+  # has_one :house
 end
