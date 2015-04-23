85172965b96d32e268b5db6cbdebacf91a0fffc2
chenxiyao <sarachenxiyao@hotmail.com>
2015-04-23 17:00:12 -0500

    commit

--- app/assets/javascripts/chores.js
+++ app/assets/javascripts/chores.js
@@ -26,13 +26,4 @@ $(document).ready(function() {
       	}
     });
   });
-
-  $(".new_chore").on("ajax:success", function(e, data, status, xhr) {
-  $("#chore_list").append(xhr.responseText);
-  $(".chore_fields").val("")
-  return console.log("stuff", xhr.responseText);
-}).on("ajax:error", function(e, xhr, status, error) {
-  console.log("Failure!!")
-  return $("#new_event").prepend("<p>ERROR</p>");
-});
 });
