f647f8df002752f5bdce27f32681870409581a9d
Morgan McCrory <morganmccrory@gmail.com>
2015-04-23 19:42:25 -0500

    jquery animations completed

--- app/assets/javascripts/fridge.js
+++ app/assets/javascripts/fridge.js
@@ -1,52 +1,33 @@
 $(document).ready(function() {
 
+  var fridge = $("#fridge")
+
   $("#new_message").on("ajax:success", function(e, data, status, xhr) {
-      $("#fridge").append(xhr.responseText);
-      $("#fridge").find(".message-table:last").hide();
-      $("#fridge").find(".message-table:last").show(1000);
+      $(fridge).append(xhr.responseText);
+      $(fridge).find(".message-table:last").hide();
+      $(fridge).find(".message-table:last").show(1000);
+      $("#new-content").val("")
+      $(".message-content-valid").hide();
+      $("#message_picture_picture_content").val("")
+    }).on("ajax:error", function(e, xhr, status, error) {
+      return $(fridge).append("<p class='message-content-valid'>Message length must be 6</p>");
+    });
 
-  $("#new-content").val("")
-  $(".message-content-valid").hide();
-  $("#message_picture_picture_content").val("")
-  return console.log("stuff", xhr.responseText);
-}).on("ajax:error", function(e, xhr, status, error) {
-  return $("#fridge").append("<p class='message-content-valid'>Message length must be 6</p>");
+  $('.delete-button').click(function(e) {
+    e.preventDefault();
+    var parent = $(this).parent().parent().parent().parent();
+    var url = $(this).attr("id")
+    $.ajax({
+      type: 'DELETE',
+      url: url,
+      beforeSend: function() {
+        parent.css("background-color", "#E6E6FA");
+      },
+      success: function() {
+        parent.slideUp(300,function() {
+          parent.remove();
+        });
+      }
+    });
+  });
 });
-});
-
-
-
-
-
-
-
-
-
-
-// $(document).on("click", "#submit-message", function(post){
-//     post.preventDefault();
-
-//     var url = window.location.pathname;
-//     var route = url.substring(url.lastIndexOf('/') - 1);
-//     var id = route.lastIndexOf('/');
-
-//     createMessage = $.ajax({
-//       url: "/houses/" + id + "/messages",
-//       method: "POST",
-//       data: $("#new-content").serialize()
-//     });
-
-//     createMes sage.done(function(response) {
-//       $("#new-content").val("").end();
-//       console.log(response);
-//       $("#fridge").append(response);
-
-//       //   var firstTweet = $("#tweets-container ul li:first")
-//       //   var animateTweet = function() {
-//       //     firstTweet.css("display", "none");
-//       //     firstTweet.slideDown()
-//       //   };
-
-//       // animateTweet();
-//     });
-//   });
--- app/assets/javascripts/houses.js
+++ app/assets/javascripts/houses.js
@@ -3,13 +3,11 @@ $(document).ready(function() {
 
   $("#edit-house-link").on("click", function(event) {
     event.preventDefault();
-    console.log(">>>>>>>>>>>>>>>>>>>>");
     $("#edit-house-form-div").css("display", "block")
   })
 
   $("#edit-house-manager-link").on("click", function(event) {
     event.preventDefault();
-    console.log(">>>>>>>>>>>>>>>>>>>>");
     $("#edit-house-manager-form-div").css("display", "block")
   })
 });
